11016bd57051a2e7c238c103fc4f75bc518fcf43
Yu Chen <106727171+chinavampire@users.noreply.github.com>
2024-11-06 15:01:42 +0800

    Update connection.php

--- generator/connection.php
+++ generator/connection.php
@@ -25,7 +25,7 @@ if (!$conn){
 
 
 
-//ini_set("error_reporting","E_ALL & ~E_NOTICE");
+ini_set("error_reporting","E_ALL & ~E_NOTICE");
 mysqli_query($conn,"set character set 'utf8'");
 mysqli_query($conn,"set names 'utf8'");
 header("Content-type: text/html; charset=utf-8");
